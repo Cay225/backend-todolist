9663a4f58e6e9f5359b90a14b8978933b938142d
Cay225 <cayyoan7@gmail.com>
2024-08-14 08:38:03 +0000

    first cpmmit

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,16 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-      mongoose.connect(process.env.DBURL, {serverSelectionTimeoutMS: 5000});
-      console.log("Database connected !!!")
+        await mongoose.connect('mongodb://localhost:27017/backend', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Database connected !!!');
     } catch (err) {
-      console.error(err.message);
-      process.exit(1);
+        console.error(err.message);
+        process.exit(1);
     }
-  };
+};
 
-  module.exports = connectDB()
+module.exports = connectDB;
